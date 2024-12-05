d4297b4d05e5e2c525f917be5160a3cdea7dde06
YulyaGeier <jagfay@gmail.com>
2024-12-05 18:59:13 +0300

    18287

---
+++ 5/16371в.py
@@ -0,0 +1,10 @@
+p=[]
+for n in range (1,1000):
+    b=bin(n)[2:]
+    if n%3==0:
+        b=b+b[:-2]
+    else: b=b+(bin((n%3)*3)[2:])
+    r=int(b,2)
+    if r >= 195:
+        p.append(r)
+print(min(p))
